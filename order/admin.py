5ac7be4a49ece152d9a7d74a2bfb6a83c461705e
AbdalrahmanAbouelnaga <abdalrahmanabouelnaga@gmail.com>
2023-01-31 03:38:20 +0200

    payment with paymob fixed

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Order,OrderItem
 # Register your models here.
+
+
+class ItemInline(admin.TabularInline):
+    model = OrderItem
+
+class OrderAdmin(admin.ModelAdmin):
+    model= Order
+    inlines=[
+        ItemInline,
+    ]
+
+admin.site.register(Order,OrderAdmin)
